a3e8ac1e62c212d8c10292f18abd27b6cd59701d
Himanshu Sangwan <himanshu.s@internshala.com>
2025-02-25 16:00:13 +0530

    try commit

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
-<?php 
-use Illuminate\Support\Facades\Route;
+<?php
+
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return File::get(public_path('/build/index.html'));
